5da1a6d84cc06853fea8ef0d1e304736fcce6794
miguel-l-b <cc22327@g.unicamp.br>
2023-04-10 16:44:49 -0300

    add parameter cep

--- src/core/use_cases/api/IGetAddressByCEP.java
+++ src/core/use_cases/api/IGetAddressByCEP.java
@@ -1,7 +1,8 @@
 package core.use_cases.api;
 
 import core.entities.Logradouro;
+import core.validations.CEP;
 
 public interface IGetAddressByCEP {
-    Logradouro GetAddressByCEP();
+    Logradouro GetAddressByCEP(String cep);
 }
