708b88b183e1197fd05fd8d0a51953df76140a9b
miguel-l-b <miguellopesbraido@gmail.com>
2023-04-10 23:43:17 -0300

    fire

--- src/app/ports/repositories/library/IUpdateLibraryByCepAndNumber.java
+++
@@ -1,7 +0,0 @@
-package app.ports.repositories.library;
-
-import core.entities.Library;
-
-public interface IUpdateLibraryByCepAndNumber {
-    Library updateLibraryByCepAndNumber(int cep, int number, Library newData);
-}
