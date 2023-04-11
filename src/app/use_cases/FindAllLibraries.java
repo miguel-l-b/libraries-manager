501d47726ce2362b5aa8ed0ed43c5cc4a7d5f3c7
miguel-l-b <cc22327@g.unicamp.br>
2023-04-11 17:42:13 -0300

    refactor and add validations

--- src/app/use_cases/FindAllLibraries.java
+++ src/app/use_cases/FindAllLibraries.java
@@ -3,55 +3,65 @@ package app.use_cases;
 import java.util.HashSet;
 import java.util.Set;
 
-import app.ports.repositories.library.*;
+import app.ports.repositories.library.IFindAllLibrariesRepository;
+import app.ports.repositories.library.IFindLibraryByCepAndNumberRepository;
+import core.entities.CEP;
 import core.entities.Library;
 import core.use_cases.library.IGetLibrary;
 import exceptions.InvalidValueException;
 
 public class FindAllLibraries implements IGetLibrary {
-	private final IFindAllLibrariesRepository REPOSITORY;
-	private final IFindLibrariesByCepRepository REPOSITORY_BY_CEP;
-	private final IFindAllLibrariesByNameRepository REPOSITORY_BY_NAME;
-	private final IFindAllLibrariesByNumberRepository REPOSITORY_BY_NUMBER;
-	private final IFindLibraryByCepAndNumberRepository REPOSITORY_BY_CEP_AND_NUMBER;
+	private final IFindAllLibrariesRepository REPOSITORY_ALL;
+	private final IFindLibraryByCepAndNumberRepository REPOSITORY_BY;
 
-	public FindAllLibraries(IFindAllLibrariesRepository repository, IFindLibrariesByCepRepository repositoryByCep,
-			IFindAllLibrariesByNameRepository repositoryByName,
-			IFindAllLibrariesByNumberRepository repositoryByNumber,
-			IFindLibraryByCepAndNumberRepository repositoryByCepAndNumber) {
-		this.REPOSITORY = repository;
-		this.REPOSITORY_BY_CEP = repositoryByCep;
-		this.REPOSITORY_BY_NAME = repositoryByName;
-		this.REPOSITORY_BY_NUMBER = repositoryByNumber;
-		this.REPOSITORY_BY_CEP_AND_NUMBER = repositoryByCepAndNumber;
+	public FindAllLibraries(IFindAllLibrariesRepository repositoryAll, IFindLibraryByCepAndNumberRepository repositoryBy) { 
+		this.REPOSITORY_ALL = repositoryAll;
+		this.REPOSITORY_BY = repositoryBy;
 	}
 
 	@Override
-	public Library[] getAllLibraries() { return this.REPOSITORY.findAllLibraries(); }
+	public Library[] getAllLibraries() {
+		return this.REPOSITORY_ALL.findAllLibraries();
+	}
 
 	@Override
-	public Library[] getLibrariesBy(int cepOrNumber) throws InvalidValueException {
-		Set<Library> result = new HashSet();
-		Library[] ceps = this.REPOSITORY_BY_CEP.findLibrariesByCep(cepOrNumber);
-		Library[] numbers = this.REPOSITORY_BY_NUMBER.findAllLibrariesByNumber(cepOrNumber);
+	public Library[] getLibrariesBy(int cepOrNumber) {
+		if(cepOrNumber <= 0)
+			throw new IllegalArgumentException(String.format("the data: \"%s\" is not a CEP or number", cepOrNumber));
+		Library[] all = this.getAllLibraries();
+		Set<Library> filtered = new HashSet<>();
 
-		for (Library library : numbers)
-			result.add(library);
+		for (Library library : all)
+			if(library.getCep() == cepOrNumber || library.getNumber() == cepOrNumber)
+				filtered.add(library);
 
-		for (Library library : ceps)
-			result.add(library);
-
-		return (Library[]) result.toArray();
+		return (Library[]) filtered.toArray();
 	}
 
 	@Override
 	public Library[] getLibrariesBy(String name) throws InvalidValueException {
-		return this.REPOSITORY_BY_NAME.findAllLibrariesByName(name);
+		if(name == null || name.isBlank())
+			throw new IllegalArgumentException(String.format("the name: \"%s\" cannot be empty", name));
+		
+		Library[] all = this.getAllLibraries();
+		Set<Library> filtered = new HashSet<>();
+
+		for (Library library : all)
+			if(library.getName().toUpperCase() == name.toUpperCase())
+				filtered.add(library);
+		
+		return (Library[]) filtered.toArray();
+		
 	}
 
 	@Override
 	public Library getLibraryBy(int cep, int number) throws InvalidValueException {
-		return this.REPOSITORY_BY_CEP_AND_NUMBER.findLibraryBy(cep, number);
+		if(!CEP.validation(cep))
+			throw new IllegalArgumentException(String.format("the cep: \"%s\" is not a CEP", cep));
+		if(number <= 0 || number > 99999)
+			throw new IllegalArgumentException(String.format("the number: \"%s\" is not a number", number));
 
+		return this.REPOSITORY_BY.findLibraryBy(cep, number);
 	}
+
 }
