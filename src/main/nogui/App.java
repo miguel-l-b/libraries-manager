83398ea9b10bebcfe8baae8e510ad8b182b0bc1a
miguel-l-b <cc22327@g.unicamp.br>
2023-04-11 17:42:56 -0300

    create main nogui

---
+++ src/main/nogui/App.java
@@ -0,0 +1,15 @@
+package main.nogui;
+
+public class App {
+    public final String PATH_DATA;
+    public final String URL_API;
+
+    public App(String pathData, String urlApi) {
+        this.PATH_DATA = pathData;
+        this.URL_API = urlApi;
+    }
+
+    public void main(String[] args) {
+        
+    }
+}
