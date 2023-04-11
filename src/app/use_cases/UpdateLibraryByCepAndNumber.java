c0254d1bc4410a327d31b7d15090f3486f25f001
miguel-l-b <cc22327@g.unicamp.br>
2023-04-11 17:42:30 -0300

    add validations

--- src/app/use_cases/UpdateLibraryByCepAndNumber.java
+++ src/app/use_cases/UpdateLibraryByCepAndNumber.java
@@ -1,6 +1,7 @@
 package app.use_cases;
 
 import app.ports.repositories.library.IUpdateLibraryByCepAndNumberRepository;
+import core.entities.CEP;
 import core.entities.Library;
 import core.use_cases.library.IUpdateLibrary;
 
@@ -11,7 +12,12 @@ public class UpdateLibraryByCepAndNumber implements IUpdateLibrary{
 	public UpdateLibraryByCepAndNumber(IUpdateLibraryByCepAndNumberRepository repository){this.REPOSITORY = repository;}
 
 	@Override
-	public void updateLibrary(Integer cep, Integer number, Library newData) { 
-		this.REPOSITORY.updateLibraryByCepAndNumber(cep, number, newData);
+	public void updateLibrary(String cep, int number, Library newData) {
+		if(!CEP.validation(cep))
+			throw new IllegalArgumentException(String.format("the cep: \"%s\" is not a CEP", cep));
+		if(number <= 0 || number > 99999)
+			throw new IllegalArgumentException(String.format("the number: \"%s\" is not a number", number));
+		try { this.REPOSITORY.updateLibraryByCepAndNumber(CEP.parseInt(cep), number, newData); }
+		catch (Exception e) { throw new IllegalArgumentException(e.getMessage()); }
 	 }
 }
