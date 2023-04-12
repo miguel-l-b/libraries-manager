28a595220e57b86abf46fc91c9b8714d88475097
miguel-l-b <cc22327@g.unicamp.br>
2023-04-12 16:46:54 -0300

    reformulate

--- src/main/nogui/Read.java
+++ src/main/nogui/Read.java
@@ -5,20 +5,44 @@ import Console.ConsoleManager;
 import Input.Keyboard;
 import Input.MultipleChoice;
 import app.use_cases.GetLibrary;
+import core.entities.CEP;
 import core.entities.Library;
+import core.entities.Logradouro;
 import exceptions.InvalidValueException;
+import infrastructure.providers.api.CEPProvider;
 import infrastructure.repositories.jackson.LibraryRepository;
 
 public class Read {
 	public final GetLibrary REPOSITORY;
+	public final CEPProvider API_CEP;
 
-	public Read(LibraryRepository repository) { this.REPOSITORY = new GetLibrary(repository, repository); }
-	public Read(GetLibrary repository) { this.REPOSITORY = repository; }
+	public Read(LibraryRepository repository, CEPProvider apiCep) {
+		this.REPOSITORY = new GetLibrary(repository, repository);
+		this.API_CEP = apiCep;
+	}
+	private Read(GetLibrary repository, CEPProvider apiCep) {
+		this.REPOSITORY = repository;
+		this.API_CEP = apiCep;
+	}
 
-	public void printError(Exception e) {
+	private void printError(Exception e) {
 		ConsoleManager.println(e.getMessage(), Colors.RED);
-		ConsoleManager.println("Pressione qualquer tecla para continuar...", Colors.RED);
+		stop();
+	}
+
+	private void stop() {
+		ConsoleManager.println("Pressione qualquer tecla para continuar...", Colors.YELLOW_BOLD);
 		Keyboard.getString();
+	}
+
+	private void printLibrary(Library data) {
+		String address;
+		try {
+			Logradouro l = API_CEP.getAddress(CEP.parseCep(data.getCep()));
+			address = String.format("%s, %d - %s, %s, %s - %s, %d", l.getLogradouro(), data.getNumber(), l.getComplemento(), l.getBairro(), l.getCidade(), l.getEstado(), data.getCep());
+		} catch (Exception e) { address = String.format("%n, %n", data.getCep(), data.getNumber()); }
+		ConsoleManager.print(data.getName()+" ");
+		ConsoleManager.println(address);
 	}
 
 	public void run() {
@@ -28,7 +52,8 @@ public class Read {
 		ConsoleManager.println("1 - Buscar por nome", Colors.CYAN);
 		ConsoleManager.println("2 - Buscar por CEP ou Numero", Colors.CYAN);
 		ConsoleManager.println("3 - Buscar por CEP e Numero", Colors.CYAN);
-
+		ConsoleManager.println("4 - Mostrar todos", Colors.CYAN);
+		ConsoleManager.println("5 - Voltar para o menu principal", Colors.CYAN);
 		try {
 			Keyboard.multipleChoiceInt(new MultipleChoice<Integer>() {
 				@Override
@@ -40,7 +65,8 @@ public class Read {
 
 							try {
 								for (Library l : REPOSITORY.getLibrariesBy(name))
-									ConsoleManager.println(l.toString(), Colors.WHITE);
+									printLibrary(l);
+								stop();
 							}
 							catch (InvalidValueException e) { printError(e); }
 							break;
@@ -50,32 +76,42 @@ public class Read {
 
 							try {
 								for (Library l : REPOSITORY.getLibrariesBy(cepOrNumber))
-									ConsoleManager.println(l.toString(), Colors.WHITE);
+									printLibrary(l);
+								stop();
 							}
 							catch (InvalidValueException e) { printError(e); }
 							break;
 						case(3):
 							ConsoleManager.print(" Cep: ", Colors.CYAN);
-							int cep = Integer.valueOf(Keyboard.getString());
+							String cep = Keyboard.getString();
 							ConsoleManager.print(" Numero: ", Colors.CYAN);
 							int number = Integer.parseInt(Keyboard.getString());
 							
 							try {
-								ConsoleManager.println(REPOSITORY.getLibraryBy(cep, number).toString(), Colors.WHITE);
+								printLibrary(REPOSITORY.getLibraryBy(cep, number));
+								stop();
 							}
 							catch (InvalidValueException e) { printError(e); }
 							break;
+						case(4):
+								for (Library l : REPOSITORY.getAllLibraries())
+									printLibrary(l);
+								stop();
+							break;
+						case(5):
+							break;
 						default:
 							printError(new InvalidValueException("Opção inválida!")); 
-							new Read(REPOSITORY).run();
+							new Read(REPOSITORY, API_CEP).run();
 							break;
 					}
 				}
 			});
 		}
-		catch (Exception e) { 
+		catch (Exception e) {
+			e.printStackTrace();
 			printError(e); 
-			new Read(REPOSITORY).run();
+			new Read(REPOSITORY, API_CEP).run();
 		}
 	}
 }
