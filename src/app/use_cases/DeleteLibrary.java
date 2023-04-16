e988872081a5018d6e6d7ede962edd6696d28489
miguel-l-b <miguellopesbraido@gmail.com>
2023-04-15 21:03:15 -0300

    refactorage

--- src/app/use_cases/DeleteLibrary.java
+++ src/app/use_cases/DeleteLibrary.java
@@ -11,12 +11,13 @@ public class DeleteLibrary implements IDeleteLibrary {
     public DeleteLibrary(IDeleteLibraryRepository repository) { this.REPOSITORY = repository; }
 
     @Override
-    public void deleteLibrary(int cep, int number) throws InvalidValueException {
-        if(!CEP.validation(cep))
+    public void deleteLibrary(String cep, int number) throws InvalidValueException {
+        if (!CEP.validation(cep))
             throw new InvalidValueException(String.format("the cep: \"%s\" is not a CEP", cep));
-        if(number <= 0 || number > 99999)
+        if (number <= 0 || number > 99999)
             throw new InvalidValueException(String.format("the number: \"%s\" is not a number", number));
-            
-        this.REPOSITORY.delete(cep, number);
+
+        try { this.REPOSITORY.delete(CEP.parseInt(cep), number); }
+        catch (Exception e) { throw new InvalidValueException(e.getMessage()); }
     }
 }
